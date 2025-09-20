ea262e44ab47f39d9d38e0daa552dc84ef028972
Mukul  Sharma <mukulsharma@Mukuls-MacBook-Air.local>
2025-09-20 12:24:29 +0530

    update2

--- hello-world/scripts/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/react-app
-npm install
-npm run build
--- hello-world/scripts/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo rm -rf /usr/share/nginx/html/*
-sudo cp -r /home/ec2-user/react-app/build/* /usr/share/nginx/html/
-sudo systemctl restart nginx
